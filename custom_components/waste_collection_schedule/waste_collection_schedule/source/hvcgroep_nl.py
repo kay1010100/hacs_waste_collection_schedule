bc5af42e66aa9726b1dd6c2a88961ae4e4828706
kay1010100 <44272417+kay1010100@users.noreply.github.com>
2026-04-12 17:02:06 +0200

    Capitalization issue fix

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/hvcgroep_nl.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/hvcgroep_nl.py
@@ -21,12 +21,6 @@ def EXTRA_INFO():
 
 
 TEST_CASES = {
-    "Hvcgroep: Heemskerk": {
-        "postal_code": "1969NB",
-        "house_number": "27",
-        "house_letter": "a",
-        "service": "hvcgroep",
-    },
     "Alphen a/d Rijn": {
         "postal_code": "2404EN",
         "house_number": "7",
@@ -320,7 +314,7 @@ class Source:
             _LOGGER.info(f"Checking {self.house_letter} {self.suffix}")
             for address in data:
                 if (
-                    address["huisletter"] == self.house_letter
+                    address["huisletter"].lower() == self.house_letter.lower()
                     and address["toevoeging"] == self.suffix
                 ):
                     bag_id = address["bagid"]
